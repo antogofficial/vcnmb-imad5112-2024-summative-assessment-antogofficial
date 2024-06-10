29a7be487cd1ce72267f114b977ed034ee66cea1
antogofficial <160749174+antogofficial@users.noreply.github.com>
2024-06-10 09:40:54 +0200

    started code

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tableLayoutWeather = findViewById<TableLayout>(R.id.tableLayoutWeather)
+
+
     }
 }
