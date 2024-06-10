6d8b556340c71487be2bb0f2984eaf59d64c9d93
antogofficial <160749174+antogofficial@users.noreply.github.com>
2024-06-10 10:47:20 +0200

    update
    Ive added a splash screen and I have also added two buttons, one to start and one to exit

--- app/src/main/java/com/example/weatherapp/MainActivityStart.kt
+++ app/src/main/java/com/example/weatherapp/MainActivityStart.kt
@@ -1,11 +1,23 @@
 package com.example.weatherapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivityStart : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_start)
+
+        val buttonStart = findViewById<Button>(R.id.buttonStart)
+
+        buttonStart.setOnClickListener() {
+
+            val intext = Intent(this, MainActivityStart::class.java)
+
+            startActivity(intent)
+
+        }
     }
 }
