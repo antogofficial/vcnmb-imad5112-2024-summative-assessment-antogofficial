2613a25c4bd9003381ba2866f5c9f715cfdca7db
antogofficial <160749174+antogofficial@users.noreply.github.com>
2024-06-10 10:04:47 +0200

    update

---
+++ app/src/main/java/com/example/weatherapp/MainActivityStart.kt
@@ -0,0 +1,22 @@
+package com.example.weatherapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivityWeather : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_Start)
+
+        val buttonStart = findViewById<Button>(R.id.buttonStart)
+
+        buttonStart.setOnClickListener() {
+
+            val intext = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
